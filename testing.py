ad3924be67cd013ccddd47e7d8dc2fa7bddb6a66
Madison Fong <mfong@ucvts.tec.nj.us>
2018-01-10 15:35:22 +0000

    With l variable

--- testing.py
+++ testing.py
@@ -13,22 +13,19 @@ class User(object):
 player = User('none', 'nowhere', 0, 0, 0, 0, 'none')
 hours = 16
 day_number = 1
+hours_to_graduate = 50
 
 l = 'the Diner'
 x = input(f"""You are on Day {day_number}. You have {hours} hours left. You are at {l}. Where would you like to go?
-    Diner (D)
-    Bank (B)
-    Career Services (C)
-    Education Center (E)
+    Diner (d)
+    Bank (b)
+    Career Services (c)
+    Education Center (e)
 """)
-while hours == 0:
-    x = input(f"""You are on Day {day_number}. You have {hours} hours left. You are at {l}. Where would you like to go?
-    Diner (D)
-    Bank (B)
-    Career Services (C)
-    Education Center (E)
-""")
-while x == 'D':
+
+player.travel(x)
+# DINER
+while x == 'd':
     if l == 'the Bank':
         hours -= 1
     if l == 'Career Services':
@@ -38,41 +35,49 @@ while x == 'D':
     l = 'the Diner'
     print(f"You have {hours} hours left. You are at {l}.")
     y = input("""What would you like to do?
-    Travel (T)
-    Purchase Food (P)
-    Work (Wo)
+    Travel (t)
+    Purchase Food (p)
+    Work (wo)
 """)
-    if y == 'T':
+    # TRAVEL
+    if y == 't':
         c = input(f"""You have {hours} hours left. Where would you like to go?
-        Bank (B)
-        Career Services (C)
-        Education Center (E)
+        Bank (b)
+        Career Services (b)
+        Education Center (e)
 """)
-        if c == 'B':
-            x = 'B'
-        if c == 'C':
-            x = 'C'
-        if c == 'E':
-            x = 'E'
-    if y == 'P':
+        if c == 'b':
+            x = 'b'
+        if c == 'c':
+            x = 'c'
+        if c == 'e':
+            x = 'e'
+    # PURCHASE
+    if y == 'p':
         price = random.randint(20, 100)
         z = input(f"""The price of food today is {price} dollars. Type B to buy and E to go back to the Diner options.
 """)
-        if z == 'B':
+        if z == 'b':
             if player.money_pockets >= price:
                 player.money_pockets -= price
+                player.food = 'food'
             else:
                 print('Sorry, you do not have enough money.')
-        if z == 'E':
-            x == 'D'
-    if y == 'Wo':
+        if z == 'e':
+            x == 'd'
+    # WORK
+    if y == 'wo':
         if player.job_loc == 'Diner' and player.job != 'none':
             a = int(input(f'You have {hours} hours left. How many hours would you like to work? '))
-            hours -= a
-            player.money_pocket += (a * player.salary)
+            if a > hours:
+                print('Sorry, you do not have enough hours in the day.')
+            else:
+                hours -= a
+                player.money_pocket += (a * player.salary)
         else:
             print('Sorry, you do not have a job here.')
-while x == 'B':
+# BANK
+while x == 'b':
     if l == 'the Diner':
         hours -= 1
     if l == 'Career Services':
@@ -82,31 +87,34 @@ while x == 'B':
     l = 'the Bank'
     print(f"You have {hours} hours left. You are at {l}.")
     y = input("""What would you like to do?
-    Travel (T)
-    Work (Wo)
-    Deposit (D)
-    Withdraw (Wi)
+    Travel (t)
+    Work (wo)
+    Deposit (d)
+    Withdraw (wi)
 """)
+    # TRAVEL
     if y == 'T':
         c = input(f"""You have {hours} hours left. Where would you like to go?
-        Diner (D)
-        Career Services (C)
-        Education Center (E)
+        Diner (d)
+        Career Services (c)
+        Education Center (e)
 """)
-        if c == 'D':
-            x = 'D'
-        if c == 'C':
-            x = 'C'
-        if c == 'E':
-            x = 'E'
-    if y == 'Wo':
+        if c == 'd':
+            x = 'd'
+        if c == 'c':
+            x = 'c'
+        if c == 'e':
+            x = 'e'
+    # WORK
+    if y == 'wo':
         if player.job_loc == 'Bank' and player.job != 'none':
             a = int(input(f'You have {hours} hours left. How many hours would you like to work? '))
             hours -= a
             player.money_pocket += (a * player.salary)
         else:
             print('Sorry, you do not have a job here.')
-    if y == 'D':
+    # DEPOSIT
+    if y == 'd':
         if player.money_pocket != 0:
             a = int(input(f"""How much money would you like to deposit?
             You are carrying {player.money_pocket} dollars.
@@ -117,7 +125,8 @@ while x == 'B':
                 player.money_bank += a
             else:
                 print('Sorry, you do not have that amount of money.')
-    if y == 'Wi':
+    # WITHDRAW
+    if y == 'wi':
         if player.money_bank != 0:
             a = int(input(f"""How much money would you like to withdraw?
             You have {player.money_bank} dollars in your bank account.
@@ -128,7 +137,8 @@ while x == 'B':
                 player.money_pocket += a
             else:
                 print('Sorry, you do not have that amount of money in the bank.')
-while x == 'C':
+# CAREER SERVICES
+while x == 'c':
     if l == 'the Diner':
         hours -= 1
     if l == 'the Bank':
@@ -138,28 +148,31 @@ while x == 'C':
     l = 'Career Services'
     print(f"You have {hours} hours left. You are at {l}.")
     y = input("""What would you like to do?
-    Travel (T)
-    View Jobs (V)
+    Travel (t)
+    View Jobs (v)
 """)
-    if y == 'T':
+    # TRAVEL
+    if y == 't':
         c = input(f"""You have {hours} hours left. Where would you like to go?
-        Diner (D)
-        Bank (B)
-        Education Center (E)
+        Diner (d)
+        Bank (b)
+        Education Center (e)
 """)
-        if c == 'D':
-            x = 'D'
-        if c == 'B':
-            x = 'B'
-        if c == 'E':
-            x = 'E'
-    if y == 'V':
+        if c == 'd':
+            x = 'd'
+        if c == 'b':
+            x = 'b'
+        if c == 'e':
+            x = 'e'
+    # VIEW JOBS
+    if y == 'v':
         d = input("""Where would you like to view jobs?
-        Diner (D)
-        Bank (B)
-        Education Center (E)
+        Diner (d)
+        Bank (b)
+        Education Center (e)
 """)
-        if d == 'D':
+        # DINER JOBS
+        if d == 'd':
             odds1 = random.randint(1, 3)
             odds2 = random.randint(1, 3)
             odds3 = random.randint(1, 3)
@@ -167,19 +180,19 @@ while x == 'C':
             salary_cook = random.randint(11, 15)
             salary_manager = random.randint(25, 31)
             e = input(f"""You have a {player.education} education. Please indicate the job you would like to apply for.
-            Cashier (Ca): ${salary_cashier} - no requirements
-            Cook (Co): ${salary_cook} - must have high school education
-            Manager (M): ${salary_manager} - must have college education
+            Cashier (ca): ${salary_cashier} - no requirements
+            Cook (co): ${salary_cook} - must have high school education
+            Manager (m): ${salary_manager} - must have college education
 """)
-            if e == 'Ca':
+            if e == 'ca':
                 if odds1 == 1:
                     player.job = 'Cashier'
                     player.job_loc = 'the Diner'
                     player.salary = salary_cashier
                 else:
                     print('Sorry, this job is not available right now.')
-            if e == 'Co':
-                if player.education == 'high school' or 'college':
+            if e == 'co':
+                if player.education != 'none':
                     if odds2 == 1:
                         player.job = 'Cook'
                         player.job_loc = 'the Diner'
@@ -188,7 +201,7 @@ while x == 'C':
                         print('Sorry, this job is not available right now.')
                 if player.education == 'none':
                     print('Sorry, you are not qualified for this job.')
-            if e == 'M':
+            if e == 'm':
                 if player.education == 'college':
                     if odds3 == 1:
                         player.job = 'Manager'
@@ -198,7 +211,8 @@ while x == 'C':
                         print('Sorry, this job is not available right now.')
                 if player.education != 'college':
                     print('Sorry, you are not qualified for this job.')
-        if d == 'B':
+        # BANK JOBS
+        if d == 'b':
             odds1 = random.randint(1, 3)
             odds2 = random.randint(1, 3)
             odds3 = random.randint(1, 3)
@@ -206,11 +220,79 @@ while x == 'C':
             salary_teller = random.randint(14, 18)
             salary_manager = random.randint(25, 31)
             e = input(f"""You have a {player.education} education. Please indicate the job you would like to apply for.
-            Security Guard (S): ${salary_guard} - no requirements
-            Teller (T): ${salary_teller} - must have high school education
-            Manager (M): ${salary_manager} - must have college education
+            Security Guard (s): ${salary_guard} - no requirements
+            Teller (t): ${salary_teller} - must have high school education
+            Manager (m): ${salary_manager} - must have college education
 """)
-while x == 'E':
+            if e == 's':
+                if odds1 == 1:
+                    player.job = 'Security Guard'
+                    player.job_loc = 'the Bank'
+                    player.salary = salary_guard
+                else:
+                    print('Sorry, this job is not available right now.')
+            if e == 't':
+                if player.education != 'none':
+                    if odds2 == 1:
+                        player.job = 'Teller'
+                        player.job_loc = 'the Bank'
+                        player.salary = salary_teller
+                    if odds2 != 1:
+                        print('Sorry, this job is not available right now.')
+                if player.education == 'none':
+                    print('Sorry, you are not qualified for this job.')
+            if e == 'm':
+                if player.education == 'college':
+                    if odds3 == 1:
+                        player.job = 'Manager'
+                        player.job_loc = 'the Bank'
+                        player.salary = salary_manager
+                    else:
+                        print('Sorry, this job is not available right now.')
+                if player.education != 'college':
+                    print('Sorry, you are not qualified for this job.')
+        # EDUCATION CENTER JOBS
+        if d == 'e':
+            odds1 = random.randint(1, 3)
+            odds2 = random.randint(1, 3)
+            odds3 = random.randint(1, 3)
+            salary_janitor = random.randint(11, 15)
+            salary_secretary = random.randint(17, 21)
+            salary_teacher = random.randint(27, 32)
+            e = input(f"""You have a {player.education} education. Please indicate the job you would like to apply for.
+            Janitor (j): ${salary_janitor} - no requirements
+            Secretary (s): ${salary_secretary} - must have high school education
+            Teacher (t): ${salary_teacher} - must have college education
+""")
+            if e == 'j':
+                if odds1 == 1:
+                    player.job = 'Janitor'
+                    player.job_loc = 'the Education Center'
+                    player.salary = salary_janitor
+                else:
+                    print('Sorry, this job is not available right now.')
+            if e == 's':
+                if player.education != 'none':
+                    if odds2 == 1:
+                        player.job = 'Secretary'
+                        player.job_loc = 'the Education Center'
+                        player.salary = salary_secretary
+                    if odds2 != 1:
+                        print('Sorry, this job is not available right now.')
+                if player.education == 'none':
+                    print('Sorry, you are not qualified for this job.')
+            if e == 't':
+                if player.education == 'college':
+                    if odds3 == 1:
+                        player.job = 'teacher'
+                        player.job_loc = 'the Education Center'
+                        player.salary = salary_teacher
+                    else:
+                        print('Sorry, this job is not available right now.')
+                if player.education != 'college':
+                    print('Sorry, you are not qualified for this job.')
+# EDUCATION CENTER
+while x == 'e':
     if l == 'the Diner':
         hours -= 2
     if l == 'the Bank':
@@ -220,28 +302,56 @@ while x == 'E':
     l = 'the Education Center'
     print(f"You have {hours} hours left. You are at {l}.")
     y = input("""What would you like to do?
-    Travel (T)
-    Work (Wo)
-    Learn (L)
+    Travel (t)
+    Work (wo)
+    Learn (l)
 """)
-    if y == 'T':
+    # TRAVEL
+    if y == 't':
         c = input(f"""You have {hours} hours left. Where would you like to go?
-        Diner (D)
-        Bank (B)
-        Career Services (C)
+        Diner (d)
+        Bank (b)
+        Career Services (c)
 """)
-        if c == 'D':
-            x = 'D'
-        if c == 'B':
-            x = 'B'
-        if c == 'C':
-            x = 'C'
-    if y == 'Wo':
+        if c == 'd':
+            x = 'd'
+        if c == 'b':
+            x = 'b'
+        if c == 'c':
+            x = 'c'
+    # WORK
+    if y == 'wo':
         if player.job_loc == 'Education Center' and player.job != 'none':
             a = int(input(f'You have {hours} hours left. How many hours would you like to work? '))
             hours -= a
             player.money_pocket += (a * player.salary)
         else:
             print('Sorry, you do not have a job here.')
+    # LEARN
+    if y == 'L':
+        if player.education == 'college':
+            print('You are at the highest education level. You cannot learn anymore.')
+        else:
+            g = int(input(f"""You have {hours} hours left. Your education level: {player.education}. You need {hours_to_graduate} more hours to graduate.
+        How many hours would you like to learn?
+"""))
+        if g > hours:
+            print('Sorry, you do not have enough hours today.')
+        else:
+            hours_to_graduate -= g
+            hours -= g
+        if hours_to_graduate == 0:
+            if player.education == 'none':
+                print('Congratulations, you have graduated high school!')
+                player.education = 'high school'
+            elif player.education == 'high school':
+                print('Congratulations, you have graduated college!')
+                player.education = 'college'
 while hours == 0:
     day_number += 1
+    x = input(f"""You are on Day {day_number}. You have {hours} hours left. You are at {l}. Where would you like to go?
+    Diner (d)
+    Bank (b)
+    Career Services (c)
+    Education Center (e)
+""")
